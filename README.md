b5e60c454dafbdd7ecaed3dc942d03deba6045e9
donielacosta1995 <donielacosta1995@gmailcom>
2021-11-22 17:31:04 -0400

    LET y CONST, Multilínea, Spread Operator y Desestructuración

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+# curso-ecmascript
 # emascript
--- src/es6/index.js
+++ src/es6/index.js
@@ -1,18 +1,87 @@
-function newFunction(name, age, country) {
-  var name = name || 'oscar';
-  var age = age || 32;
-  var country = country || 'MX';
-  console.log(name, age, country);
+// function newFunction(name, age, country) {
+//   var name = name || 'oscar';
+//   var age = age || 32;
+//   var country = country || 'MX';
+//   console.log(name, age, country);
+// }
+
+// // es6
+// function newFunction2(name = 'oscar', age = 32, country = "MX") {
+//   console.log(name, age, country);
+// };
+
+// newFunction2();
+// newFunction2('Ricardo', '23', 'CO');
+
+// let hello = "Hello";
+// let world = "World";
+// let epicPhrase = hello + ' ' + world;
+// console.log(epicPhrase);
+// let epicPhrase2 = `${hello} ${world}`;
+// console.log(epicPhrase2);
+
+// let lorem = "Qui consequatur. Commodi. Ipsum vel duis yet minima \n"
+//   + "otra frase epica que necesitamos."
+
+// // es6
+// let lorem2 = `otra frase epica que necesitamos
+// ahora es otra frase epica
+// `;
+
+// console.log(lorem);
+// console.log(lorem2);
+
+// let person = {
+//   'name': 'oscar',
+//   'age': 32,
+//   'country': 'MX'
+// }
+
+// console.log(person.name, person.age);
+
+// let { name } = person;
+// console.log(name);
+
+// let team1 = ['Oscar', 'Julian', 'Ricardo'];
+// let team2 = ['Valeria', 'Yesica', 'Camila'];
+
+// let education = ['David', ...team1, ...team2];
+
+// console.log(education);
+
+// {
+//   var globalVar = "Global Var";
+// }
+
+// {
+//   let globalLet = 'Global Let';
+//   console.log(globalLet);
+// }
+
+// console.log(globalVar);
+
+// const a = 'b';
+// a = 'a';
+// console.log(a);
+
+function newFunction(name,age,country){
+  var name = name || 'Doniel';
+  var age = age || 26;
+  var country = country || 'Venezuela';
+  console.log(name,age,country);
 }
 
-// es6
-function newFunction2(name = 'oscar', age = 32, country = "MX") {
-  console.log(name, age, country);
-};
+//ES6
+
+function newFunction2(name='Doniel',age = 26, country ='Venezuela'){
+  console.log(name,age,country);
+}
 
 newFunction2();
-newFunction2('Ricardo', '23', 'CO');
+newFunction2('lina','24','Merida');
 
+
+//
 let hello = "Hello";
 let world = "World";
 let epicPhrase = hello + ' ' + world;
@@ -20,32 +89,34 @@ console.log(epicPhrase);
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
 
-let lorem = "Qui consequatur. Commodi. Ipsum vel duis yet minima \n"
-  + "otra frase epica que necesitamos."
+let loren = "Aprendiendo EmaScript \n" + "Con PLATZI ";
 
-// es6
-let lorem2 = `otra frase epica que necesitamos
-ahora es otra frase epica
+//es6
+let loren2 = `OTRA QUE NECESITAMOS
+AHORA ES NUEVA FRASE
 `;
 
-console.log(lorem);
-console.log(lorem2);
+console.log(loren);
+console.log(loren2);
 
 let person = {
-  'name': 'oscar',
-  'age': 32,
-  'country': 'MX'
+  'name': 'Doniel',
+  'age': 23,
+  'country': 'Venezuela'
 }
 
 console.log(person.name, person.age);
 
-let { name } = person;
+
+let{ name, age, country }= person;
+
 console.log(name);
 
-let team1 = ['Oscar', 'Julian', 'Ricardo'];
-let team2 = ['Valeria', 'Yesica', 'Camila'];
 
-let education = ['David', ...team1, ...team2];
+let team1 = ['Doniel', 'Julian', 'Miguel'];
+let team2 = ['Ruben', 'Maibe', 'Fatima'];
+
+let education = ['Andres', ...team1, ...team2];
 
 console.log(education);
 
@@ -54,12 +125,13 @@ console.log(education);
 }
 
 {
-  let globalLet = 'Global Let';
+  let globalLet ='Global Let';
   console.log(globalLet);
 }
 
 console.log(globalVar);
 
 const a = 'b';
+
 a = 'a';
 console.log(a);
