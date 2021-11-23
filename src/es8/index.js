fbb626a1c5321a0d3080b2313f21c9a8199963f8
donielacosta1995 <donielacosta1995@gmailcom>
2021-11-23 12:16:36 -0400

    - Object.entries: convertir un objeto a un matriz donde los propiedades de objeto clave, valor se transformarán en un arreglo [clave,valor]  **- Object.values: **devuelve los valores de un objeto convertidos en un arreglo.  - “texto”.padStart() | “texto”.padEnd()

--- src/es8/index.js
+++ src/es8/index.js
@@ -6,3 +6,19 @@ const data = {
 
 const entries = Object.entries(data);
 console.log(entries);
+console.log(entries.length);
+
+const datas = {
+    frontend: 'Doniel',
+    backend: 'Lina',
+    desing:'ROxana'
+}
+
+const values = Object.values(datas);
+console.log(values);
+
+
+const string = 'hello';
+console.log(string.padStart(7,'co'));
+console.log(string.padEnd(12,'------'));
+console.log('food'.padEnd(12,'------'));
