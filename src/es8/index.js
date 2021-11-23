be4b36b7f855f4e5c668e1c52af73acbaa8aa2d6
donielacosta1995 <donielacosta1995@gmailcom>
2021-11-23 14:33:13 -0400

    Async Await

--- src/es8/index.js
+++ src/es8/index.js
@@ -22,3 +22,29 @@ const string = 'hello';
 console.log(string.padStart(7,'co'));
 console.log(string.padEnd(12,'------'));
 console.log('food'.padEnd(12,'------'));
+
+const helloWorld = () => {
+    return new Promise((resolve, reject)=>{
+        (false) ? setTimeout(()=>resolve('Hello WORLD'),3000)
+        : reject(new Error('TEST ERROR'))
+    })
+};
+
+const helloAsync = async ()=>{
+    const hello = await helloWorld();
+    console.log(hello);
+};
+
+helloAsync();
+
+
+const anothorFunction = async()=>{
+    try{
+        const hello = await helloWorld();
+        console.log(hello);
+    }catch(error){
+        console.log(error);
+    }
+};
+
+anothorFunction();
