254c0630f5eec4b8e6a0b3a2b36a6365a3e6addb
Doniel Acosta <donielacosta1995@gmail.com>
2023-10-06 20:23:34 -0400

    .

---
+++ src/es6/default-params.js
@@ -0,0 +1,9 @@
+function newUser(name,age,country){
+    var name = name || 'Doniel';
+    var age = age || 28;
+    var country = country || 'Merida';
+    console.log(name,age,country);
+}
+
+newUser();
+newUser('LINA',25,'CARACAS');
